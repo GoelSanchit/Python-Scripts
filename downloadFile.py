d086a23b2ba917cf971a178296fc32f5f831d0d8
Sanchit Goel <sanchit.mgoel007@gmail.com>
2015-10-25 21:52:31 +0530

    Update downloadFile.py

--- downloadFile.py
+++ downloadFile.py
@@ -2,7 +2,7 @@ from urllib import request
 
 goog_url = 'http://real-chart.finance.yahoo.com/table.csv?s=GOOG&d=6&e=18&f=2015&g=d&a=2&b=27&c=2014&ignore=.csv'
 
-def download_any_file(csv_url):
+def download_any_file(csv_url)
     response = request.urlopen(csv_url)
 	csv = response.read()
 	csv_str = str(csv)
